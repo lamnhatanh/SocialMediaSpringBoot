c2f084d523e03c27d12828ea75256c46b2ca67ee
Brian Home <anhlam051595@gmail.com>
2024-01-08 22:58:43 -0500

    add template-engine

--- src/main/java/com/brian/socialmedia/util/EmailConstructor.java
+++ src/main/java/com/brian/socialmedia/util/EmailConstructor.java
@@ -2,11 +2,14 @@ package com.brian.socialmedia.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.thymeleaf.TemplateEngine;
 
 public class EmailConstructor {
 
 	@Autowired
 	private Environment env;
+	
+	private TemplateEngine templateEngine;
 	
 	
 	// Use javax mail and thymeleaf
